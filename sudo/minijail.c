022ecf51cbebfb65d79bdc1c3ff30e5f54101530
SupeChicken666 <me@supechicken666.dev>
2024-10-11 13:21:09 +0000

    sudo
    Signed-off-by: SupeChicken666 <me@supechicken666.dev>

---
+++ sudo/minijail.c
@@ -0,0 +1,19 @@
+int minijail_no_new_privs(){
+  return 0;
+}
+
+int minijail_add_fs_restriction_rx(){
+  return 0;
+}
+
+int minijail_add_fs_restriction_advanced_rw(){
+  return 0;
+}
+
+int minijail_set_enable_profile_fs_restrictions(){
+  return 0;
+}
+
+int minijail_enable_default_fs_restrictions(){
+  return 0;
+}
